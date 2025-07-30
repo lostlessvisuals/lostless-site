8695267df75e1be56009c6bb0dcbb09b34ef821c
lostlessvisuals <lostless.visuals@gmail.com>
2025-07-29 19:46:51 -0700

    updated gallery with 31 videos and images

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".gallery-grid");
+  const msnry = new Masonry(grid, {
+    itemSelector: ".gallery-item",
+    columnWidth: ".gallery-item",
+    percentPosition: true,
+    gutter: 16
+  });
+
+  imagesLoaded(grid).on("progress", () => {
+    msnry.layout();
+  });
+});
